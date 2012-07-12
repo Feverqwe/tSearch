74b93ab2b758e3ed6525579a91beb91f6e679781
Антон <@192.168.0.3>
2012-07-12 13:44:29 +0600

    В процессе

--- js/history.js
+++ js/history.js
@@ -1,6 +1,13 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+$(function (){
+    $('input.sbutton.main').click(function (){
+        var s = (document.URL).replace(/(.*)history.html/,'');
+        if (s!= '')
+            var s = s.replace(/#back=(.*)/,'$1');
+        window.location = '/index.html#s='+s;
+    });
+    $('div.rm_btn').click(function (){
+        $(this).parents().eq(1).hide('fast', function () {
+            var id = $(this).parents().eq(1).data('id');
+        });
+    });
+});
