fd34306d4af3a4dd947739c22f69ba6842db20a9
anton <anton@192.168.3.4>
2012-08-16 10:32:47 +0600

    мелочи

--- js/ad.js
+++ js/ad.js
@@ -24,15 +24,15 @@ var ad = function () {
         else {
             $('#ad').empty();
             last_ad_id = i;
+            if (i == 1)
+                add_ad('http://www.ozon.ru/PartnerTwinerNew.aspx?revident=4d58bed8-b5c6-4424-bd04-4a2a06e6d98b');
+            else if (i == 2)
+                add_ad('http://www.ozon.ru/PartnerTwinerNew.aspx?revident=369828f4-0fc9-4262-b20c-07449a5c0bc0');
+            else if (i == 3)
+                add_ad('http://www.ozon.ru/PartnerTwinerNew.aspx?revident=5ac92f2d-1ca1-4e47-bc0d-adc7182824b4');
+            else
+                add_ad('http://www.ozon.ru/PartnerTwinerNew.aspx?revident=1d7850fe-0d87-44cb-ba2a-5bf1a1d887cb');
         }
-        if (i = 1)
-            add_ad('http://www.ozon.ru/PartnerTwinerNew.aspx?revident=4d58bed8-b5c6-4424-bd04-4a2a06e6d98b');
-        else if (i = 2)
-            add_ad('http://www.ozon.ru/PartnerTwinerNew.aspx?revident=369828f4-0fc9-4262-b20c-07449a5c0bc0');
-        else if (i = 3)
-            add_ad('http://www.ozon.ru/PartnerTwinerNew.aspx?revident=5ac92f2d-1ca1-4e47-bc0d-adc7182824b4');
-        else
-            add_ad('http://www.ozon.ru/PartnerTwinerNew.aspx?revident=1d7850fe-0d87-44cb-ba2a-5bf1a1d887cb');
     }
     return {
         insert : function () {
