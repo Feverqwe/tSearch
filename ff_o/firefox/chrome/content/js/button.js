4c446ad1baec5d188c4e6bc8818307407ad3e14c
anton <anton@192.168.3.4>
2012-09-05 14:10:24 +0600

    Фикс кнопки, фф - гавно

--- ff_o/firefox/chrome/content/js/button.js
+++ ff_o/firefox/chrome/content/js/button.js
@@ -4,3 +4,30 @@ var run = function () {
     var recentWindow = wm.getMostRecentWindow("navigator:browser");
     recentWindow.delayedOpenTab('chrome://TorrentsMultiSearch/content/index.html#s=', null, null, null, null);
 }
+var addButton = function () {
+    var startButtonId =  "torrentsmultysearch-button";
+    var navBarId = "nav-bar";
+    var navBar = document.getElementById(navBarId);
+    var currentSet = navBar.currentSet;
+	
+    // Append only if the button is not already there.
+    var curSet = currentSet.split(",");
+    if (curSet.indexOf(startButtonId) == -1)
+    {
+        navBar.insertItem(startButtonId);
+        navBar.setAttribute("currentset", navBar.currentSet);
+        document.persist("nav-bar", "currentset");
+	
+        try
+        {
+            // The current global scope is not browser.xul.
+            top.BrowserToolboxCustomizeDone(true);
+        }
+        catch (e)
+        {
+	   
+        }
+	            
+    }
+}
+window.addEventListener("load",addButton,false);
