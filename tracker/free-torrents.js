440e4be3d2237dba723c24bfec6b7570ccc2ebaf
anton <anton@192.168.3.4>
2012-09-27 10:38:01 +0600

    fix free-torrents

--- tracker/free-torrents.js
+++ tracker/free-torrents.js
@@ -64,13 +64,8 @@ tracker[tmp_num] = function () {
                 xhr.abort();
             xhr = $.ajax({
                 type: 'POST',
-                url: url,
+                url: url+'?nm='+encode(text),
                 cache : false,
-                data: {
-                    'max' : 1,
-                    'to' : 1,
-                    'nm' : text
-                },
                 success: function(data) {
                     view.result(id,readCode(data),t);
                 },
@@ -79,6 +74,39 @@ tracker[tmp_num] = function () {
                     view.auth(0,id);
                 }
             });
+        }
+        var encode = function (sValue) {
+            var    text = "", Ucode, ExitValue, s;
+            for (var i = 0; i < sValue.length; i++) {
+                s = sValue.charAt(i);
+                Ucode = s.charCodeAt(0);
+                var Acode = Ucode;
+                if (Ucode > 1039 && Ucode < 1104) {
+                    Acode -= 848;
+                    ExitValue = "%" + Acode.toString(16);
+                }
+                else if (Ucode == 1025) {
+                    Acode = 168;
+                    ExitValue = "%" + Acode.toString(16);
+                }
+                else if (Ucode == 1105) {
+                    Acode = 184;
+                    ExitValue = "%" + Acode.toString(16);          
+                } 
+                else if (Ucode == 32) {
+                    Acode = 32;
+                    ExitValue = "%" + Acode.toString(16);          
+                } 
+                else if (Ucode == 10){
+                    Acode = 10;
+                    ExitValue = "%0A";
+                }
+                else {
+                    ExitValue = s;          
+                }
+                text = text + ExitValue; 
+            }      
+            return text; 
         }
         return {
             getPage : function (a) {
