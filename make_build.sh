63f290ac1a884cb4bb3c6dabf4c5f48ea8bae1c3
Anton <anton@iMac-Anton.local>
2013-04-17 20:57:23 +0600

    .

--- make_build.sh
+++ make_build.sh
@@ -22,7 +22,7 @@ cp ./compressed/index.html ./build/.
 cp ./compressed/magic.html ./build/.
 cp ./compressed/options.html ./build/.
 cp ./compressed/manifest.json ./build/.
-#>tr compression
+
 
 rm ./build/css/*.css
 
@@ -34,7 +34,7 @@ rm ./build/js/jqcloud-1.0.2.min.js
 rm ./build/js/jquery.tablesorter.js
 rm ./build/js/lang.js
 rm ./build/js/storage.js
-
+#>tr compression
 java -jar compiler.jar --js ./compressed/torrent_lib.js --js ./tracker/anidub.js --js ./tracker/katushka.js --js ./tracker/rustorka.js --js ./tracker/bestrepack.js --js ./tracker/kickass.js --js ./tracker/rutor.js --js ./tracker/bigfangroup.js --js ./tracker/kinozal.js --js ./tracker/rutracker.js --js ./tracker/bitsnoop.js --js ./tracker/libertorrent.js --js ./tracker/tapochek.js --js ./tracker/brodim.js --js ./tracker/megashara.js --js ./tracker/tfile.js --js ./tracker/btdigg.js --js ./tracker/mininova.js --js ./tracker/thepiratebay.js --js ./tracker/evrl.js --js ./tracker/mmatracker.js --js ./tracker/thepiratebay2.js --js ./tracker/extratorrent.js --js ./tracker/my-hit.js --js ./tracker/torrentino.js --js ./tracker/fast-torrent.js --js ./tracker/nnm-club.js --js ./tracker/torrentmac.js --js ./tracker/fenopy.js --js ./tracker/opensharing.js --js ./tracker/torrents.freedom.js --js ./tracker/filebase.js --js ./tracker/opentorrent.js --js ./tracker/torrents.local.js --js ./tracker/free-torrents.js --js ./tracker/piratbit.js --js ./tracker/torrentz.js --js ./tracker/hdclub.js --js ./tracker/piratca.js --js ./tracker/underverse.js --js ./tracker/hurtom.js --js ./tracker/pornolab.js --js ./tracker/x-torrents.js --js ./tracker/inmac.js --js ./tracker/rgfootball.js --js ./tracker/youtracker.js --js ./tracker/isohunt.js --js ./tracker/riperam.js --js_output_file ./build/js/torrent_lib.js
 #<tr compression
 java -jar compiler.jar --js ./js/storage.js --js ./js/lang.js --js ./js/background.js --js_output_file ./build/js/background.js
@@ -42,8 +42,6 @@ java -jar compiler.jar --js ./js/storage.js --js ./js/lang.js --js ./js/history.
 java -jar compiler.jar --js ./js/jquery.tablesorter.js --js ./js/jqcloud-1.0.2.min.js --js ./js/apprise-1.5.js --js ./js/storage.js --js ./js/lang.js --js ./js/engine.js --js ./js/view.js --js ./js/explore.js --js ./js/ad.js --js ./js/counter.js --js_output_file ./build/js/view.js
 java -jar compiler.jar --js ./js/storage.js --js ./js/lang.js --js ./js/magic.js --js_output_file ./build/js/magic.js
 java -jar compiler.jar --js ./js/storage.js --js ./js/lang.js --js ./js/engine.js --js ./js/options.js --js_output_file ./build/js/options.js
-
-#<chrome comression
 
 cp ./css/stylesheet.css ./build/css/stylesheet.css
 cat  ./css/jqcloud.css >> ./build/css/stylesheet.css
@@ -53,6 +51,9 @@ java -jar yuicompressor-2.4.7.jar  ./build/css/stylesheet.css -o ./build/css/sty
 java -jar yuicompressor-2.4.7.jar ./css/options.css -o ./build/css/options.css
 java -jar yuicompressor-2.4.7.jar ./css/history.css -o ./build/css/history.css
 java -jar yuicompressor-2.4.7.jar ./css/magic.css -o ./build/css/magic.css
+#<chrome comression
+
+
 
 #firefox
 mkdir ./build_firefox/chrome
