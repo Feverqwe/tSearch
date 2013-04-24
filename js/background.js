7415a33366f358d8da81ac8ca7bdb3d2e7f65b53
anton9121 <anton9121@gmail.com>
2013-04-24 22:25:44 +0600

    fix context menu update

--- js/background.js
+++ js/background.js
@@ -1,40 +1,39 @@
-var bg = function () {
-    var add_in_omnibox = function () {
+var bg = function() {
+    var add_in_omnibox = function() {
         var add_in_omnibox = (GetSettings('add_in_omnibox') !== undefined) ? parseInt(GetSettings('add_in_omnibox')) : true;
         if (add_in_omnibox) {
             chrome.omnibox.onInputEntered.addListener(function(text) {
                 chrome.tabs.create({
-                    "url":"index.html#s=" + text,
-                    "selected":true
+                    "url": "index.html#s=" + text,
+                    "selected": true
                 });
             });
         }
     }
-    var update_context_menu = function () {
+    var update_context_menu = function() {
         var context_menu = (GetSettings('context_menu') !== undefined) ? parseInt(GetSettings('context_menu')) : true;
-        if (context_menu)
-            chrome.contextMenus.removeAll(function () {
-                if (context_menu) {
-                    chrome.contextMenus.create({
-                        "type" : "normal",
-                        "id" : "item",
-                        "title" : _lang.ctx_title,
-                        "contexts" : ["selection"],
-                        "onclick" : function (info) {
-                            chrome.tabs.create({
-                                "url":"index.html#s=" + info.selectionText,
-                                "selected":true
-                            });
-                        }
-                    });
-                }
-            });
+        chrome.contextMenus.removeAll(function() {
+            if (context_menu) {
+                chrome.contextMenus.create({
+                    "type": "normal",
+                    "id": "item",
+                    "title": _lang.ctx_title,
+                    "contexts": ["selection"],
+                    "onclick": function(info) {
+                        chrome.tabs.create({
+                            "url": "index.html#s=" + info.selectionText,
+                            "selected": true
+                        });
+                    }
+                });
+            }
+        });
     }
     return {
-        add_in_omnibox : function () {
+        add_in_omnibox: function() {
             return add_in_omnibox();
         },
-        update_context_menu : function () {
+        update_context_menu: function() {
             return update_context_menu();
         }
     }
