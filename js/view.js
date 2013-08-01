6940751f381634cf551b4dafaa94f626de7d5ba8
Anton <anton9121@gmail.com>
2013-08-01 07:51:03 +0600

    unlock version

--- js/view.js
+++ js/view.js
@@ -1384,8 +1384,3 @@ $(window).on('hashchange', function() {
         global_wl_hash = location.hash;
     }
 });
-
-window.LOCKR_SETTINGS = {
-    time: 15, 
-    siteName: 'Torrents MultiSearch'
-  };
