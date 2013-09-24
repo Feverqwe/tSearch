4eebc59abb30d370c5212a56326c076c8b368735
Anton <anton9121@gmail.com>
2013-09-24 09:15:44 +0600

    upd

--- ff_o/opera/build/js/storage.js
+++ ff_o/opera/build/js/storage.js
@@ -1,10 +1,7 @@
-var SetSettings = function (key,value) {
+var SetSettings = function(key, value) {
     localStorage[key] = value;
     return value;
 };
-var GetSettings = function (key) {
-    if (localStorage[key] === undefined)
-        return undefined;
-    else
-        return localStorage[key];
+var GetSettings = function(key) {
+    return localStorage[key];
 };
