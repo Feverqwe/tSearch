340b958a80f3a9372c473625fdca1dc8acb4658f
Anton <anton9121@gmail.com>
2014-02-27 10:30:18 +0600

    1

--- js/storage.js
+++ js/storage.js
@@ -1,29 +1,31 @@
-var allow_favorites_sync = 1;
-var SetSettings = function(key, value) {
-    if (value === undefined) {
-        delete localStorage[key];
+(function(){
+    var allow_favorites_sync = 1;
+    window.SetSettings = function(key, value) {
+        if (value === undefined) {
+            delete localStorage[key];
+            return value;
+        }
+        if (key === "exp_cache_favorites" && allow_favorites_sync && chrome.storage) {
+            var data = {};
+            data[key] = value;
+            chrome.storage.sync.set(data);
+        }
+        localStorage[key] = value;
         return value;
-    }
-    if (key === "exp_cache_favorites" && allow_favorites_sync && chrome.storage) {
-        var data = {};
-        data[key] = value;
-        chrome.storage.sync.set(data);
-    }
-    localStorage[key] = value;
-    return value;
-};
-var GetSettings = function(key) {
-    if (key === "exp_cache_favorites" && allow_favorites_sync && chrome.storage) {
-        chrome.storage.sync.get(key, function(data) {
-            if (data.exp_cache_favorites === undefined) {
-                return;
-            }
-            if (localStorage[key] !== data.exp_cache_favorites) {
-                localStorage[key] = data.exp_cache_favorites;
-                explore.updateFavorites(data.exp_cache_favorites);
-            }
-        });
-    }
-    return localStorage[key];
-};
-allow_favorites_sync = parseInt(GetSettings('allow_favorites_sync') || 1);
+    };
+    window.GetSettings = function(key) {
+        if (key === "exp_cache_favorites" && allow_favorites_sync && chrome.storage) {
+            chrome.storage.sync.get(key, function(data) {
+                if (data.exp_cache_favorites === undefined) {
+                    return;
+                }
+                if (localStorage[key] !== data.exp_cache_favorites) {
+                    localStorage[key] = data.exp_cache_favorites;
+                    explore.updateFavorites(data.exp_cache_favorites);
+                }
+            });
+        }
+        return localStorage[key];
+    };
+    allow_favorites_sync = parseInt(GetSettings('allow_favorites_sync') || 1);
+})();
