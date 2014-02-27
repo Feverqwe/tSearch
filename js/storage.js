dca2a182d93b395599ef22adc1832df716dfaa7c
Anton <anton9121@gmail.com>
2014-02-27 17:13:24 +0600

    Revert "extend storage"
    This reverts commit 0d75fc6205fe9911df91b05547a10376cdd2d706.

--- js/storage.js
+++ js/storage.js
@@ -1,5 +1,5 @@
 (function(){
-    var sync_items = ['exp_cache_favorites'];
+    var allow_favorites_sync = 1;
     window.SetSettings = function(key, value) {
         if (value === undefined) {
             delete localStorage[key];
@@ -11,60 +11,19 @@
     window.GetSettings = function(key) {
         return localStorage[key];
     };
-    window.SetStorageSettings = function(local, cb) {
-        var sync = undefined;
-        sync_items.forEach(function(item){
-            if (local.hasOwnProperty(item)) {
-                if (sync === undefined) {
-                    sync = {};
-                }
-                sync[item] = local[item];
-                delete local[item];
-            }
-        });
-        chrome.storage.local.set(local, function(){
-            if (sync !== undefined) {
-                chrome.storage.sync.set(sync, cb);
-            } else {
-                cb();
-            }
-        });
-    };
-    window.GetStorageSettings = function(local, cb) {
-        if (typeof local === 'string') {
-            if ( sync_items.indexOf(local) !== -1 ) {
-                chrome.storage.sync.get(local, cb);
-            } else {
-                chrome.storage.local.get(local, cb);
-            }
-            return;
+    window.SetStorageSettings = function(keys, cb) {
+        var type = 'local';
+        if (allow_favorites_sync && keys.exp_cache_favorites !== undefined) {
+            type = 'sync';
         }
-        var sync = undefined;
-        sync_items.forEach(function(item){
-            if ( sync_items.indexOf(item) !== -1 ) {
-                if (sync === undefined) {
-                    sync = [];
-                }
-                sync.push(item);
-                local.splice(local.indexOf(item), 1);
-            }
-        });
-        chrome.storage.local.get(local, function(obj) {
-            if (sync !== undefined) {
-                chrome.storage.sync.get(sync, function(sObj) {
-                    sync_items.forEach(function(item) {
-                        if (sObj.hasOwnProperty(item)) {
-                            obj[item] = sObj[item];
-                        }
-                    });
-                    cb(obj);
-                });
-                return;
-            }
-            cb(obj);
-        });
+        chrome.storage[type].set(keys, cb);
     };
-    if (parseInt(GetSettings('allow_favorites_sync') || 1) === 0) {
-        sync_items.splice(sync_items.indexOf('exp_cache_favorites'), 1);
-    }
+    window.GetStorageSettings = function(keys, cb) {
+        var type = 'local';
+        if (allow_favorites_sync && keys === 'exp_cache_favorites') {
+            type = 'sync';
+        }
+        chrome.storage[type].get(keys, cb);
+    };
+    allow_favorites_sync = parseInt(GetSettings('allow_favorites_sync') || 1);
 })();
