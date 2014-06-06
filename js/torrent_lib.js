1e569ed867c83944da35aa8a84b70885b716e3f9
Anton <anton9121@gmail.com>
2014-06-06 14:45:19 +0600

    fix

--- js/torrent_lib.js
+++ js/torrent_lib.js
@@ -6,7 +6,7 @@ window.torrent_lib = {};
     var trackers = ['anidub', 'bestrepack', 'bigfangroup', 'bitsnoop', 'brodim', 'btdigg', 'evrl', 'extratorrent',
         'fast-torrent', 'fenopy', 'filebase', 'free-torrents', 'hdclub', 'hurtom', 'inmac', 'katushka', 'kickass',
         'kinozal', 'libertorrent', 'megashara', 'mininova', 'nnm-club', 'opensharing', 'opentorrent', 'piratbit',
-        'piratca', 'rgfootball', 'riperam', 'ru-board', 'rustorka', 'rutor', 'rutracker', 'tapochek', 'tfile',
+        'piratca', 'rgfootball', 'riperam', 'ru-board', 'rutor', 'rutracker', 'tapochek', 'tfile',
         'thepiratebay', 'torrentmac', 'torrents.freedom', 'torrents.local', 'torrentz', 'underverse', 'x-torrents'];
     var s = document.getElementsByTagName('script')[0];
     trackers.forEach(function (src) {
