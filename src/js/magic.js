aa5a9b93829d6791c7238916c87edd582e2139f4
Anton <anton9121@gmail.com>
2015-04-22 13:11:09 +0500

    magic

---
+++ src/js/magic.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Anton on 22.04.2015.
+ */
+var magic = {
+    domCache: {
+        container: document.getElementById('container'),
+        frame: document.getElementById('frame')
+    },
+    once: function() {
+        "use strict";
+
+    }
+};
+
+engine.init(function() {
+    "use strict";
+    magic.once();
+});
