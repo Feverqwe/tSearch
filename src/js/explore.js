5a93fe0bae4ef3cc3940be73fff5602354a16b9e
Anton <anton9121@gmail.com>
2015-05-04 11:33:06 +0500

    1

--- src/js/explore.js
+++ src/js/explore.js
@@ -106,160 +106,6 @@ var explore = {
             proxyUrl: 'https://images-pos-opensocial.googleusercontent.com/gadgets/proxy?container=pos&resize_w=220&rewriteMime=image/jpeg&url='
         }
     },
-    getCacheDate: function(keepAlive) {
-        "use strict";
-        if (!keepAlive) {
-            return undefined;
-        }
-        var currentDate = new Date();
-        var day = currentDate.getDay();
-        var hours = currentDate.getHours();
-        var minutes = currentDate.getMinutes();
-        var seconds = currentDate.getSeconds();
-        var lastDay = 0;
-        keepAlive.forEach(function(num) {
-            if (day >= num) {
-                lastDay =  num;
-            }
-        });
-        day = day - lastDay;
-        return parseInt(currentDate.getTime() / 1000) - day*24*60*60 - hours*60*60 - minutes*60 - seconds;
-    },
-    getTopListColumnCount: function() {
-        "use strict";
-        var width = document.body.clientWidth;
-        return width > 1275 ? 4 : 3;
-    },
-    writeTopList: function(content) {
-        "use strict";
-        if (!content || !content.length) return;
-
-        var columnCount = this.getTopListColumnCount();
-
-        var lineIcon;
-        var lineIconStyle;
-        var column = 1;
-        var line = 1;
-        var request;
-        var body = mono.create('ul', {
-            class: 'c' + columnCount
-        });
-        for (var i = 0, item; item = content[i]; i++) {
-            if (line > 10) {
-                break;
-            }
-            lineIcon = undefined;
-            if (column % columnCount === 0) {
-                lineIconStyle = ['info'];
-                if (line < 6) {
-                    lineIconStyle.push('t' + line);
-                }
-                lineIcon = mono.create('div', {
-                    class: lineIconStyle
-                });
-            }
-            request = item.text;
-            if (item.year > 0) {
-                request += ' ' + item.year;
-            }
-            body.appendChild(mono.create('li', {
-                class: ['l'+line],
-                append: [
-                    lineIcon,
-                    mono.create('span', {
-                        title: request,
-                        append: [
-                            mono.create('a', {
-                                href: '#?search=' + encodeURIComponent(request),
-                                text: item.text
-                            })
-                        ]
-                    })
-                ]
-            }));
-            if (column % columnCount === 0) {
-                line++;
-            }
-            column++;
-        }
-
-        this.domCache.topSearch.textContent = '';
-        this.domCache.topSearch.appendChild(body);
-
-        this.varCache.topListColumnCount = columnCount;
-    },
-    loadTopList: function() {
-        "use strict";
-        var cache = engine.topList;
-
-        this.writeTopList(cache.content);
-
-        var date = this.getCacheDate([0,1,2,3,4,5,6]);
-        if (cache.keepAlive === date || !navigator.onLine) {
-            return;
-        }
-
-        mono.ajax({
-            url: "http://static.tms.mooo.com/top.json",
-            dataType: 'json',
-            cache: false,
-            success: function(data) {
-                if (data && data.keywords) {
-                    (cache.content = data.keywords).sort(function(a, b) {
-                        if (a.weight === b.weight) {
-                            return 0;
-                        } else
-                        if (a.weight > b.weight) {
-                            return -1;
-                        }
-                        return 1;
-                    });
-
-                    this.writeTopList(cache.content);
-
-                    cache.keepAlive = date;
-                    mono.storage.set({topList: cache});
-                }
-            }.bind(this)
-        });
-    },
-    show: function() {
-        "use strict";
-        if (!this.varCache.isHidden) return;
-        this.varCache.isHidden = true;
-
-        this.once();
-        this.domCache.container.classList.remove('hide');
-    },
-    hide: function() {
-        "use strict";
-        if (this.varCache.isHidden) return;
-        this.varCache.isHidden = false;
-
-        this.domCache.container.classList.add('hide');
-    },
-    writeCategoryContent: function () {
-        "use strict";
-    },
-    limitObjSize: function(obj, count) {
-        "use strict";
-        var i;
-        var arr = [];
-        for (i in obj) {
-            if (obj.hasOwnProperty(i) === false) {
-                continue;
-            }
-            arr.push(i);
-        }
-        var arr_len = arr.length;
-        if (arr_len < count) {
-            return;
-        }
-        var end = arr_len - count;
-        for (i = 0; i < end; i++) {
-            delete obj[arr[i]];
-        }
-    },
     content_parser: function () {
         var prepareObj = function(obj) {
             for (var key in item) {
@@ -699,6 +545,160 @@ var explore = {
             }
         }
     }(),
+    getCacheDate: function(keepAlive) {
+        "use strict";
+        if (!keepAlive) {
+            return undefined;
+        }
+        var currentDate = new Date();
+        var day = currentDate.getDay();
+        var hours = currentDate.getHours();
+        var minutes = currentDate.getMinutes();
+        var seconds = currentDate.getSeconds();
+        var lastDay = 0;
+        keepAlive.forEach(function(num) {
+            if (day >= num) {
+                lastDay =  num;
+            }
+        });
+        day = day - lastDay;
+        return parseInt(currentDate.getTime() / 1000) - day*24*60*60 - hours*60*60 - minutes*60 - seconds;
+    },
+    getTopListColumnCount: function() {
+        "use strict";
+        var width = document.body.clientWidth;
+        return width > 1275 ? 4 : 3;
+    },
+    writeTopList: function(content) {
+        "use strict";
+        if (!content || !content.length) return;
+
+        var columnCount = this.getTopListColumnCount();
+
+        var lineIcon;
+        var lineIconStyle;
+        var column = 1;
+        var line = 1;
+        var request;
+        var body = mono.create('ul', {
+            class: 'c' + columnCount
+        });
+        for (var i = 0, item; item = content[i]; i++) {
+            if (line > 10) {
+                break;
+            }
+            lineIcon = undefined;
+            if (column % columnCount === 0) {
+                lineIconStyle = ['info'];
+                if (line < 6) {
+                    lineIconStyle.push('t' + line);
+                }
+                lineIcon = mono.create('div', {
+                    class: lineIconStyle
+                });
+            }
+            request = item.text;
+            if (item.year > 0) {
+                request += ' ' + item.year;
+            }
+            body.appendChild(mono.create('li', {
+                class: ['l'+line],
+                append: [
+                    lineIcon,
+                    mono.create('span', {
+                        title: request,
+                        append: [
+                            mono.create('a', {
+                                href: '#?search=' + encodeURIComponent(request),
+                                text: item.text
+                            })
+                        ]
+                    })
+                ]
+            }));
+            if (column % columnCount === 0) {
+                line++;
+            }
+            column++;
+        }
+
+        this.domCache.topSearch.textContent = '';
+        this.domCache.topSearch.appendChild(body);
+
+        this.varCache.topListColumnCount = columnCount;
+    },
+    loadTopList: function() {
+        "use strict";
+        var cache = engine.topList;
+
+        this.writeTopList(cache.content);
+
+        var date = this.getCacheDate([0,1,2,3,4,5,6]);
+        if (cache.keepAlive === date || !navigator.onLine) {
+            return;
+        }
+
+        mono.ajax({
+            url: "http://static.tms.mooo.com/top.json",
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                if (data && data.keywords) {
+                    (cache.content = data.keywords).sort(function(a, b) {
+                        if (a.weight === b.weight) {
+                            return 0;
+                        } else
+                        if (a.weight > b.weight) {
+                            return -1;
+                        }
+                        return 1;
+                    });
+
+                    this.writeTopList(cache.content);
+
+                    cache.keepAlive = date;
+                    mono.storage.set({topList: cache});
+                }
+            }.bind(this)
+        });
+    },
+    show: function() {
+        "use strict";
+        if (!this.varCache.isHidden) return;
+        this.varCache.isHidden = true;
+
+        this.once();
+        this.domCache.container.classList.remove('hide');
+    },
+    hide: function() {
+        "use strict";
+        if (this.varCache.isHidden) return;
+        this.varCache.isHidden = false;
+
+        this.domCache.container.classList.add('hide');
+    },
+    writeCategoryContent: function () {
+        "use strict";
+    },
+    limitObjSize: function(obj, count) {
+        "use strict";
+        var i;
+        var arr = [];
+        for (i in obj) {
+            if (obj.hasOwnProperty(i) === false) {
+                continue;
+            }
+            arr.push(i);
+        }
+        var arr_len = arr.length;
+        if (arr_len < count) {
+            return;
+        }
+        var end = arr_len - count;
+        for (i = 0; i < end; i++) {
+            delete obj[arr[i]];
+        }
+    },
     xhr_dune: function(type, source) {
         "use strict";
         source.xhr_wait_count--;
