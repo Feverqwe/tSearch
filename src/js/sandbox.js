6486d77c6198ff7e79aae24352cff046305fc0cf
Anton <anton9121@gmail.com>
2017-01-03 01:05:32 +0500

    1

--- src/js/sandbox.js
+++ src/js/sandbox.js
@@ -6,7 +6,60 @@ require.config({
     baseUrl: './js'
 });
 require(['./min/promise.min', './min/jquery-3.1.1.min'], function (Promise) {
-    (function (Transport, runCode) {
+    (function (runCode) {
+        var Transport = function (transport) {
+            var emptyFn = function () {
+            };
+            var onceFn = function (cb, scope) {
+                return function () {
+                    if (cb) {
+                        var context = scope || this;
+                        cb.apply(context, arguments);
+                        cb = null;
+                    }
+                };
+            };
+
+            var callbackId = 0;
+            var callbackIdCallback = {};
+
+            this.onMessage = function (cb) {
+                transport.onMessage(function (msg) {
+                    if (msg.responseId) {
+                        return callbackIdCallback[msg.responseId](msg.message);
+                    }
+
+                    var response;
+                    if (msg.callbackId) {
+                        response = onceFn(function (message) {
+                            transport.sendMessage({
+                                responseId: msg.callbackId,
+                                message: message
+                            });
+                        });
+                    } else {
+                        response = emptyFn;
+                    }
+                    var result = cb(msg.message, response);
+                    if (result !== true) {
+                        response();
+                    }
+                });
+            };
+            this.sendMessage = function (message, callback) {
+                var msg = {
+                    message: message
+                };
+                if (callback) {
+                    msg.callbackId = ++callbackId;
+                    callbackIdCallback[msg.callbackId] = function (message) {
+                        delete callbackIdCallback[msg.callbackId];
+                        callback(message);
+                    };
+                }
+                transport.sendMessage(msg);
+            };
+        };
         var transport = new Transport({
             sendMessage: function (msg) {
                 parent.postMessage(msg, '*');
@@ -124,59 +177,7 @@ require(['./min/promise.min', './min/jquery-3.1.1.min'], function (Promise) {
             }
             return parsedUrl.path + value;
         };
-    })(function (transport) {
-        var emptyFn = function () {
-        };
-        var onceFn = function (cb, scope) {
-            return function () {
-                if (cb) {
-                    var context = scope || this;
-                    cb.apply(context, arguments);
-                    cb = null;
-                }
-            };
-        };
-
-        var callbackId = 0;
-        var callbackIdCallback = {};
-
-        this.onMessage = function (cb) {
-            transport.onMessage(function (msg) {
-                if (msg.responseId) {
-                    return callbackIdCallback[msg.responseId](msg.message);
-                }
-
-                var response;
-                if (msg.callbackId) {
-                    response = onceFn(function (message) {
-                        transport.sendMessage({
-                            responseId: msg.callbackId,
-                            message: message
-                        });
-                    });
-                } else {
-                    response = emptyFn;
-                }
-                var result = cb(msg.message, response);
-                if (result !== true) {
-                    response();
-                }
-            });
-        };
-        this.sendMessage = function (message, callback) {
-            var msg = {
-                message: message
-            };
-            if (callback) {
-                msg.callbackId = ++callbackId;
-                callbackIdCallback[msg.callbackId] = function (message) {
-                    delete callbackIdCallback[msg.callbackId];
-                    callback(message);
-                };
-            }
-            transport.sendMessage(msg);
-        };
-    }, function (code) {
+    })(function (code) {
         eval(code);
     });
 });
