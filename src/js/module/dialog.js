1eefb4345f8dd0cbf288dbf79b1e1187cd155649
Anton <anton9121@gmail.com>
2017-01-09 14:50:26 +0500

    dialog

---
+++ src/js/module/dialog.js
@@ -0,0 +1,53 @@
+define(function () {
+    var Dialog = function () {
+        var self = this;
+        var prefix = 'dialog';
+        var inputList = [];
+
+        var getLayer = function () {
+            var el = document.createElement('div');
+            el.classList.add(prefix + '__layer');
+            return el;
+        };
+        var getBody = function () {
+            var el = document.createElement('div');
+            el.classList.add(prefix + '__body');
+            return el;
+        };
+        var closeEvent = function (e) {
+            if (!layer.contains(e.target)) {
+                self.destroy();
+            }
+        };
+
+        var body = this.body = getBody();
+        var layer = this.layer = getLayer();
+
+        this.destroy = function () {
+            document.removeEventListener('click', closeEvent, true);
+            layer.parentNode.removeChild(layer);
+        };
+
+        this.show = function () {
+            layer.appendChild(body);
+            document.addEventListener('click', closeEvent, true);
+            document.body.appendChild(layer);
+        };
+
+        this.addInput = function (input) {
+            inputList.push(input);
+            return input;
+        };
+        this.getValues = function () {
+            var keyValue = {};
+            inputList.forEach(function (input) {
+                var value = input.value.trim();
+                if (value) {
+                    keyValue[input.name] = input.value;
+                }
+            });
+            return keyValue;
+        };
+    };
+    return Dialog;
+});
