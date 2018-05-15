48b06f44345e0d5e7ae5e88a213ad1fc67210b44
Anton <leonardspbox@gmail.com>
2018-05-15 09:38:18 +0500

    trackers.json as object

--- buildTrackersJson.js
+++ buildTrackersJson.js
@@ -1,9 +1,19 @@
+import getTrackerCodeMeta from "./src/js/tools/getTrackerCodeMeta";
+
 const fs = require('fs-extra');
 const path = require('path');
 
 const buildTrackersJson = () => {
-  return fs.readdir(path.join(__dirname, 'src', 'trackers')).then(files => {
-    const trackers = files.filter(filename => /.+\.js$/.test(filename)).map(filename => path.basename(filename, path.extname(filename)));
+  const place = path.join(__dirname, 'src', 'trackers');
+  return fs.readdir(place).then(files => {
+    const trackerIds = files.filter(filename => /.+\.js$/.test(filename)).map(filename => path.basename(filename, path.extname(filename)));
+    const trackers = {};
+    return Promise.all(trackerIds.map(id => {
+      return fs.readFile(path.join(place, `${id}.js`)).then(code => {
+        trackers[id] = getTrackerCodeMeta(code.toString()).version;
+      });
+    })).then(() => trackers);
+  }).then(trackers => {
     return fs.writeJson(path.join(__dirname, 'src', 'trackers.json'), trackers, {
       spaces: 2
     });
