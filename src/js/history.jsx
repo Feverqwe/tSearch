5690cda8fe0a9be29c6b8ac3a1ec0108f6c6f8e9
Anton <leonardspbox@gmail.com>
2018-05-21 19:13:56 +0500

    history

---
+++ src/js/history.jsx
@@ -0,0 +1,142 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import historyModel from "./models/history";
+import '../css/history.less';
+import {observer} from 'mobx-react';
+import highlight from "./tools/highlight";
+import {unixTimeToString} from "./tools/unixTimeTo";
+
+const qs = require('querystring');
+
+@observer class HistoryPage extends React.Component {
+  render() {
+    let content = null;
+    switch (this.props.store.state) {
+      case 'loading': {
+        content = ('Loading...');
+        break;
+      }
+      case 'done': {
+        content = (
+          <History {...this.props}/>
+        );
+        break;
+      }
+    }
+
+    return (
+      <div>
+        <div key="head" className="body__head">
+          <div className="search">
+            <h1 className="title">{chrome.i18n.getMessage('history')}</h1>
+          </div>
+          <div className="menu">
+            <a href="index.html" className="menu__btn menu__btn-main" title={chrome.i18n.getMessage('main')}/>
+          </div>
+        </div>
+        <div className="main">
+          {content}
+        </div>
+      </div>
+    );
+  }
+}
+
+@observer class History extends React.Component {
+  render() {
+    const /**HistoryM*/store = this.props.store;
+
+    const queries = store.getHistorySortByTime().map(query => {
+      return (
+        <HistoryQuery key={query.query} {...this.props} query={query}/>
+      );
+    });
+    return (
+      <div className="history">
+        {queries}
+      </div>
+    );
+  }
+}
+
+@observer class HistoryQuery extends React.Component {
+  constructor() {
+    super();
+
+    this.handleRemove = this.handleRemove.bind(this);
+  }
+  handleRemove(e) {
+    e.preventDefault();
+    const /**HistoryM*/store = this.props.store;
+    const /**QueryM*/query = this.props.query;
+    store.removeQuery(query.query);
+    store.save();
+  }
+  render() {
+    const /**QueryM*/query = this.props.query;
+
+    const links = query.getClicksSortByTime().map(link => {
+      return (
+        <HistoryQueryLink key={link.url} {...this.props} link={link}/>
+      );
+    });
+
+    const link = 'index.html#/search?' + qs.stringify({
+      query: query.query
+    });
+
+    return (
+      <div className="history__item">
+        <div className="item item-query">
+          <a className="item__remove" data-action="remove-query" href="#remove"
+             title={chrome.i18n.getMessage('remove')}
+             onClick={this.handleRemove}
+          />
+          <a className="item__link" href={link}>{query.query}</a>
+        </div>
+        <div className="click_history item__click_history">
+          {links}
+        </div>
+      </div>
+    );
+  }
+}
+
+@observer class HistoryQueryLink extends React.Component {
+  constructor() {
+    super();
+
+    this.handleRemove = this.handleRemove.bind(this);
+  }
+  handleRemove(e) {
+    e.preventDefault();
+    const /**HistoryM*/store = this.props.store;
+    const /**QueryM*/query = this.props.query;
+    const /**ClickM*/link = this.props.link;
+    query.removeClick(link.url);
+    store.save();
+  }
+  render() {
+    const /**QueryM*/query = this.props.query;
+    const /**ClickM*/link = this.props.link;
+
+    return (
+      <div className="item item-click_history">
+        <a className="item__remove" data-action="remove-click_history" href="#remove"
+           title={chrome.i18n.getMessage('remove')}
+           onClick={this.handleRemove}
+        />
+        <span className="item__date">
+          {unixTimeToString(link.time)}
+        </span>
+        {highlight.getReactComponent('a', {
+          className: 'item__link',
+          target: '_blank',
+          href: link.url
+        }, link.title, link.getTitleHighlightMap(query.getQueryHighlightMap()))}
+      </div>
+    );
+  }
+}
+
+window.app = ReactDOM.render(<HistoryPage store={historyModel.create()}/>, document.getElementById('root'));
--- src/js/models/history.js
+++ src/js/models/history.js
@@ -1,5 +1,7 @@
 import {types, getSnapshot} from "mobx-state-tree";
 import promisifyApi from "../tools/promisifyApi";
+import highlight from "../tools/highlight";
+import _isEqual from "lodash.isequal";
 
 const debug = require('debug')('historyModel');
 const promiseLimit = require('promise-limit');
@@ -15,6 +17,12 @@ const oneLimit = promiseLimit(1);
  * @property {function(string)} setQuery
  * @property {function(string,string,string,string)} setClick
  * Views:
+ * @property {function(string):QueryM} getOrCreateQuery
+ * @property {function:Promise} save
+ * @property {function:QueryM[]} getHistory
+ * @property {function:QueryM[]} getHistorySortByTime
+ * @property {function(string):Promise} onQuery
+ * @property {function(string, string, string, string):Promise} onClick
  */
 
 /**
@@ -26,6 +34,7 @@ const oneLimit = promiseLimit(1);
  * @property {number} time
  * Actions:
  * Views:
+ * @property {function(RegExp):Array} getTitleHighlightMap
  */
 
 const clickModel = types.model('click', {
@@ -33,6 +42,12 @@ const clickModel = types.model('click', {
   title: types.string,
   trackerId: types.string,
   time: types.number,
+}).views(self => {
+  return {
+    getTitleHighlightMap(queryHighlightMap) {
+      return highlight.getTextMap(self.title, queryHighlightMap);
+    }
+  };
 });
 
 /**
@@ -43,7 +58,12 @@ const clickModel = types.model('click', {
  * @property {number} time
  * @property {Map<string,ClickM>} clicks
  * Actions:
+ * @property {function(string,string,string,string)} setClick
+ * @property {function(string)} removeClick
  * Views:
+ * @property {function:ClickM[]} getClicks
+ * @property {function:ClickM[]} getClicksSortByTime
+ * @property {function:RegExp} getQueryHighlightMap
  */
 
 const queryModel = types.model('query', {
@@ -51,7 +71,7 @@ const queryModel = types.model('query', {
   count: types.optional(types.number, 0),
   time: types.optional(types.number, 0),
   clicks: types.optional(types.map(clickModel), {})
-}).actions(self => {
+}).actions(/**QueryM*/self => {
   return {
     setClick(query, title, url, trackerId) {
       self.clicks.set(url, {
@@ -61,13 +81,30 @@ const queryModel = types.model('query', {
         time: Math.trunc(Date.now() / 1000)
       });
     },
+    removeClick(url) {
+      self.clicks.delete(url);
+    }
+  };
+}).views(/**QueryM*/self => {
+  return {
+    getClicks() {
+      return Array.from(self.clicks.values());
+    },
+    getClicksSortByTime() {
+      return self.getClicks().sort(({time: a}, {time: b}) => {
+        return a === b ? 0 : a > b ? -1 : 1;
+      });
+    },
+    getQueryHighlightMap() {
+      return highlight.getMap(self.query);
+    },
   };
 });
 
 const historyModel = types.model('historyModel', {
   state: types.optional(types.string, 'idle'), // idle, loading, done
   history: types.optional(types.map(queryModel), {}),
-}).actions(self => {
+}).actions(/**HistoryM*/self => {
   return {
     assign(obj) {
       Object.assign(self, obj);
@@ -85,9 +122,25 @@ const historyModel = types.model('historyModel', {
 
       self.save();
     },
+    removeQuery(query) {
+      self.history.delete(query);
+    },
   };
-}).views(self => {
+}).views(/**HistoryM*/self => {
   let readyPromise = null;
+
+  const handleHistoryChangeListener = (changes, namespace) => {
+    if (namespace === 'local') {
+      const change = changes.history;
+      if (change) {
+        const history = change.newValue;
+        if (!_isEqual(history, getSnapshot(self.history))) {
+          self.assign({history});
+        }
+      }
+    }
+  };
+
   return {
     get readyPromise() {
       return readyPromise;
@@ -111,6 +164,11 @@ const historyModel = types.model('historyModel', {
     getHistory() {
       return Array.from(self.history.values());
     },
+    getHistorySortByTime() {
+      return self.getHistory().sort(({time: a}, {time: b}) => {
+        return a === b ? 0 : a > b ? -1 : 1;
+      });
+    },
     onQuery(query) {
       return self.readyPromise.then(() => {
         self.setQuery(query);
@@ -126,12 +184,16 @@ const historyModel = types.model('historyModel', {
       return readyPromise = promisifyApi('chrome.storage.local.get')({
         history: {}
       }).then(storage => {
+        chrome.storage.onChanged.addListener(handleHistoryChangeListener);
         self.assign(storage);
       }).catch(err => {
         debug('afterCreate error', err);
       }).then(() => {
         self.assign({state: 'done'});
       });
+    },
+    beforeDestroy() {
+      chrome.storage.onChanged.removeListener(handleHistoryChangeListener);
     }
   };
 });
