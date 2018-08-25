92ac9659d943647bc581d469ee9047bdbc29936d
Anton <leonardspbox@gmail.com>
2018-08-25 17:25:27 +0500

    1

--- srcDraft/js/components/SearchFrag.jsx
+++ srcDraft/js/components/SearchFrag.jsx
@@ -1,7 +1,7 @@
 import {observer} from 'mobx-react';
 import React from 'react';
 import Table from './Table';
-import getLogger from "../tools/getLogger";
+import getLogger from "../../../src/tools/getLogger";
 
 const qs = require('querystring');
 
