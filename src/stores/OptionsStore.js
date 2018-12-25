c5e623500288393ec6573ccc6ac06f6a8385d16d
Anton <leonardspbox@gmail.com>
2018-12-25 13:46:48 +0500

    1

--- src/stores/OptionsStore.js
+++ src/stores/OptionsStore.js
@@ -169,7 +169,7 @@ const OptionsStore = types.model('OptionsStore', {
         if (date < 10) {
           date = `0${date}`;
         }
-        let month = nowDate.getMonth();
+        let month = nowDate.getMonth() + 1;
         if (month < 10) {
           month = `0${month}`;
         }
